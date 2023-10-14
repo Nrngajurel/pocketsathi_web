d7a98be78956942d3f992ec7996567fe00f14682
shivaharigaj <shivaharigaj@gmail.com>
2023-10-15 02:04:53 +0545

    c

--- routes/web.php
+++ routes/web.php
@@ -23,17 +23,29 @@ Route::get('/', function () {
     // $helpRequest->notifyNearby();
 
     // send firebase notification to ctyuTOYKSD-M_BofnBOaAQ:APA91bFMiDSRbRcHxNCjNfJeveBFrlnl9AcUuu2HTua8w7CXClq8iDGqKYRdRSQI0Hz0Ueued8GxfASsqKD2haFqOP4lwKbA3s0w1jm9Pw7EdMqJkLyU_E1FxmBAHfWdnxWA-zrEpd2R token
-    $firebase = (new Factory)
-        ->withServiceAccount(public_path('pocketsathi-firebase-adminsdk-9s4u4-01c9a943c8.json'));
-    $messaging = $firebase->createMessaging();
 
-    $message = CloudMessage::withTarget('token', 'egbvp3XuTeaxH8whpTKQ4i:APA91bGnkyxQu_CunDLRmJM45v47e2gka4PSCi8NZZpUoxrTvMwt914MVb1P7kw0jsPU_hRXvu8fIesm-6dEmfkt2WxPvmgQckWZGdoC5AI_EjA2IN7oZ-MG0PNYl-UA_Q9Hhl79cbOX')
-        ->withNotification([
-            'title' => 'Help Request Nearby',
-            'body' => 'A help request is nearby. Can you assist?'
-        ]);
+    User::whereNotNull('fcm_token')->get()->each(function ($user) {
+        try {
+            $firebase = (new Factory)
+                ->withServiceAccount(public_path('pocketsathi-firebase-adminsdk-9s4u4-01c9a943c8.json'));
+            $messaging = $firebase->createMessaging();
 
-    $messaging->send($message);
+            $message = CloudMessage::withTarget('token', $user->fcm_token)
+                ->withNotification([
+                    'title' => 'Help Request Nearby',
+                    'body' => 'A help request is nearby. Can you assist?'
+                ]);
+
+            $messaging->send($message);
+
+            dump('sent to ' . $user->name);
+
+        } catch (\Throwable $th) {
+            dump('error sending to ' . $user->name);
+            //throw $th;
+        }
+
+    });
     // $user = User::first();
 
     // $nearByRequests = HelpRequest::nearby($user->lat, $user->long, 10)->get();
